5c4b0a1a37a293ea35083b2ef8bdc10200dcbec2
Abner Filipe <abner.f.c.r@hotmail.com>
2022-09-12 00:59:11 -0300

    Corrigindo lint e mudando step de testes do ci

--- library_back/accounts/serializers.py
+++ library_back/accounts/serializers.py
@@ -10,8 +10,6 @@ class UserSerializer(ModelSerializer):
 
 class RegisterSerializer(ModelSerializer):
     password = CharField(max_length=128, min_length=6, write_only=True)
-
-
     class Meta:
         model = User
         fields = ('username', 'email', 'first_name', 'last_name', 'password')
